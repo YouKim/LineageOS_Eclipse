614fa578f16767b615c9f9b0556744fb86721f05
U-dolzzo-PC\dolzzo <dolzzo@mutecsoft.com>
2017-01-24 15:40:34 +0900

    Extract java Eclipse IDE resources from tree and Configure classpath.
    Classpath setup from scratch. It's minimal setup to
    build framework/base and service core.

---
+++ cplineage.sh
@@ -0,0 +1,35 @@
+LINK_SOURCE=~/lineage
+LINK_TARGET=~/eclipse/java/lineage
+
+cd $LINK_SOURCE
+mkdir -p $LINK_TARGET
+
+find libcore -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+
+find external/icu -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/protobuf -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/tagsoup -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/nist-sip -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/junit -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/hamcrest -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/guava -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/libphonenumber -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/lzma -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/jsr305 -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find external/apache-http -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+
+find frameworks -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+
+find out -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find out -name 'classes-jarjar.jar' -exec cp -u --parents {} $LINK_TARGET  \;
+
+find . -wholename ./prebuilts*android-support-v*.jar -exec cp -u --parents {} $LINK_TARGET  \;
+
+#linageOS specific parts
+
+find vendor/cmsdk/sdk -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+find vendor/codeaurora -name '*.java' -exec cp -u --parents {} $LINK_TARGET  \;
+
+
+#find frameworks -name 'AndroidManifest.xml' -exec cp -u --parents {} $LINK_TARGET  \;
+#find -name 'Android.mk' -exec cp -u --parents {} $LINK_TARGET  \;
